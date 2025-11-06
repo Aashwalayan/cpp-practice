2d7fbede22cda678477a82a7faef3a43fa0d926f
Aashwalayan <aashwalayanjha@gmail.com>
2025-11-06 12:15:45 +0530

    added new files

---
+++ object.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class dog{
+    public:
+        string name;
+        int age;
+        string breed;
+
+        void bark(){
+            cout << "Barking!\n";
+        }
+
+        void playFetch(){
+            cout << "Playing Fetch\n";
+        }
+
+        void sleep(){
+            cout << "Sleeping\n";
+        }
+        
+};
+
+int main(){
+
+}
